82cb54abbbe08449bb38d9f15034e1e0f75f8e66
Han Yin <naco.siren.2012@gmail.com>
2017-03-20 21:21:35 -0500

    Jsoup framework ready. (/lib/HanSoup.jar: A modified version of Jsoup by me to preserve the linebreaks.)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("https://www.google.com").get();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        String title = doc.title();
+
+        return;
     }
 }
