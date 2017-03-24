3673d2b731c0f43dd28917c4ee571a783ede31df
Han Yin <naco.siren.2012@gmail.com>
2017-03-23 21:58:41 -0500

    Modified Jsoup so that each node in the DOM tree contains a String of its tree structure in the APTED input format.

---
+++ src/APTEDNodeVisitor.java
@@ -0,0 +1,36 @@
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+import org.jsoup.select.NodeVisitor;
+
+/**
+ * Created by naco_siren on 3/23/17.
+ */
+public class APTEDNodeVisitor implements NodeVisitor {
+    @Override
+    public void head(Node node, int depth) {
+//        if (node instanceof Element)
+//            System.out.println("Enter Element: " + ((Element) node).tag() + ", " + depth);
+//        else
+//            System.out.println("Enter Text: " + ((TextNode) node).text() + ", " + depth);
+    }
+
+    @Override
+    public void tail(Node node, int depth) {
+
+        if (node instanceof Element) {
+            Element element = (Element) node;
+
+            /* Adding its children's apt string to it */
+            StringBuilder stringBuilder = new StringBuilder("{" + element.tagName());
+            for (Element child : element.children()){
+                stringBuilder.append(child.APTEDTreeStructure);
+            }
+            stringBuilder.append("}");
+
+            element.APTEDTreeStructure = stringBuilder.toString();
+
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,49 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.jsoup.select.NodeTraversor;
+
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         /* 1. Jsoup parsing HTML part */
+        //part1();
 
+        /* 2. Transforming Jsoup's tree into APTED project's input format */
+
+        /** Html1:
+         *         body
+         *       /     \
+         *      A.      B.
+         *    /  \     / \
+         *  A.1 A.2  B.1 B.2
+         */
+        String html1 =
+                "<html><body><div>A.<div>A.1</div><div>A.2</div></div><div>B.<div>B.1</div><div>B.2</div></div></body></html>\n";
+        String tree1 = part2(html1);
+
+        /** Html2:
+         *         body
+         *       /     \
+         *      A.     B.
+         *     /     /  \  \
+         *   A.1   B.1 B.2 B.3
+         */
+        String html2 =
+                "<html><body><div>A.<div>A.1</div></div><div>B.<div>B.1</div><div>B.2</div><div>B.3</div></div></body></html>\n";
+        String tree2 = part2(html2);
+
+
+        /* 3. Tree Edit Distance part */
+        int diff = part3(tree1, tree2);
+        System.out.println("Difference: " + diff);
+
+        return;
+    }
+
+    public static void part1(){
         Document doc = null;
         try {
             // a. Use Jsoup directly connect to and parse a given URL
@@ -28,21 +64,31 @@ public class Main {
         String title = doc.title();
         Element resultsRoot = doc.getElementById("gs_ccl_results");
         Elements results = resultsRoot.children();
+    }
+
+    public static String part2(String html) throws Exception{
+        Document document = Jsoup.parse(html);
 
 
-        /* 2. Tree Edit Distance part */
+        APTEDNodeVisitor aptedNodeVisitor = new APTEDNodeVisitor();
+        NodeTraversor traversor = new NodeTraversor(aptedNodeVisitor);
+        traversor.traverse(document);
+
+        return document.body().APTEDTreeStructure;
+    }
+
+
+    public static int part3(String tree1, String tree2){
 
         // Parse the input.
         BracketStringInputParser parser = new BracketStringInputParser();
-        Node<StringNodeData> t1 = parser.fromString("{div{span}{div{a}{a}}}");
-        Node<StringNodeData> t2 = parser.fromString("{div{span}{div{div}}{a}}");
+        Node<StringNodeData> t1 = parser.fromString(tree1);     //"{div{span}{div{a}{a}}}");
+        Node<StringNodeData> t2 = parser.fromString(tree2);     //"{div{span}{div{div}}{a}}");
 
         // Initialise APTED.
         APTED<StringUnitCostModel, StringNodeData> apted = new APTED<>(new StringUnitCostModel());
 
         // This cast is safe due to unit cost.
-        int result = (int)apted.computeEditDistance(t1, t2);
-
-        return;
+        return  (int)apted.computeEditDistance(t1, t2);
     }
 }
