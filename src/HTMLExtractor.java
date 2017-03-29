e759f6466aff7b3af3fc3af8e23d4b68d1791039
Han Yin <naco.siren.2012@gmail.com>
2017-03-29 15:26:01 -0500

    Ver 0.2 Framework of HTMLDataExtractor class tested.

--- src/HTMLExtractor.java
+++ src/HTMLExtractor.java
@@ -16,131 +16,6 @@ import java.util.ArrayList;
  * Created by haosun on 3/27/17.
  */
 public class HTMLExtractor {
-    private Node root;
-    private float proximity = 0.7f;
-    private ArrayList<ArrayList<Node>> records = new ArrayList<ArrayList<Node>>();
-    private BracketStringInputParser APTEDParser = new BracketStringInputParser();
-    private APTED<StringUnitCostModel, StringNodeData> apted = new APTED<>(new StringUnitCostModel());
-
-    public HTMLExtractor(Node root) {
-        this.root = root;
-    }
-
-    public void setProximity(float proximity) {
-        this.proximity = proximity;
-    }
-
-    public double getProximity() {
-        return proximity;
-    }
-
-    /**
-     * The node's children will vote for each other based on proximity. If two
-     * children are close enough, they will give each other one vote. If one c-
-     * hild get enough votes, its vote result is true. That means it is one of
-     * the possible records which the user is interested in.
-     * @param node
-     * @return the vote result for each child.
-     */
-    private boolean[] vote(Node node) {
-        int numChilds = node.childNodeSize();
-        float voteThreshhold = 0.6f;
-        int[] voteCount = new int[numChilds];
-        boolean[] voteResult = new boolean[numChilds];
-
-        for(int i = 0; i < numChilds; i++) {
-            for(int j = i + 1; j < numChilds; j++) {
-                if(node.childNode(i).APTEDTreeStructure == null || node.childNode(j).APTEDTreeStructure == null) {
-                    continue;
-                }
-                apted.node.Node<StringNodeData> t1 = APTEDParser.fromString(node.childNode(i).APTEDTreeStructure);
-                apted.node.Node<StringNodeData> t2 = APTEDParser.fromString(node.childNode(j).APTEDTreeStructure);
-                float tmp = (node.childNode(i).numOffSpring + node.childNode(j).numOffSpring) / 2
-                        * (1 - proximity);
-                //System.out.println("i"+i+"j"+j+"tmp"+tmp+"distance"+apted.computeEditDistance(t1, t2));
-                if(tmp >= apted.computeEditDistance(t1, t2)) {
-                    voteCount[i]++;
-                    voteCount[j]++;
-                }
-            }
-        }
-
-        for(int i = 0; i < numChilds; i++) {
-            System.out.print(voteCount[i]);
-            if(voteCount[i] >= voteThreshhold * numChilds) {
-                voteResult[i] = true;
-            }else {
-                voteResult[i] = false;
-            }
-        }
-
-        return voteResult;
-    }
-
-    /**
-     * Decide whether a node's children are the records to output. If they are,
-     * they will be put in an arraylist. And the arraylist will be one element
-     * of the class' member records.
-     * @param node whose children are examined.
-     * @return the number of records to output. If it is 0, this node's children
-     * are not the records users are interested in.
-     */
-    private int recordsAdd(Node node) {
-        ArrayList<Node> outputList = new ArrayList<Node>();
-        int numChilds = node.childNodeSize();
-        boolean[] voteResult;
-
-        System.out.println(node.numOffSpring + ":");
-
-        voteResult = vote(node);
-        for(int i = 0; i < numChilds; i++) {
-            if(voteResult[i]) {
-                outputList.add(node.childNode(i));
-            }
-        }
-
-        if (outputList.isEmpty()) {
-            return 0;
-        } else {
-            records.add(outputList);
-            return outputList.size();
-        }
-
-    }
-
-    /**
-     * Traverse the tree (pre-order) to extract records from the HTML doc.
-     * Call method on root, if its children are hit records, move to its
-     * Sibling. If not, call method on all its child recursively one by one.
-     * @param root the root node of the DOM representation of the HTML doc.
-     */
-    public void recordsExtract(Node root) {
-        if(root.childNodeSize() > 0) {
-            if(root.childNodeSize() >= 2) {
-                if(recordsAdd(root) > 0) {
-                    return;
-                }else {
-                    for(Node node : root.childNodes()) {
-                        recordsExtract(node);
-                    }
-                }
-            }else {
-                //only one child
-                recordsExtract(root.childNode(0));
-            }
-        }
-        return;
-    }
-
-    public void filterByNumber(int minRecordsNum) {
-        for(int i = 0; i < records.size(); i++) {
-            if(records.get(i).size() < minRecordsNum) {
-                records.remove(i);
-                i--;
-            }
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         String[] removeTagNames = new String[]{"meta", "script", "title", "style"};
         String URL = "https://scholar.google.com/scholar?hl=en&q=database&as_sdt=1%2C14&as_sdtp=&oq=";
@@ -154,11 +29,182 @@ public class HTMLExtractor {
         nodeTraversor.traverse(mainBody);
 
         HTMLExtractor htmlExtractor = new HTMLExtractor(mainBody);
-        htmlExtractor.recordsExtract(htmlExtractor.root);
+        htmlExtractor.extractData(htmlExtractor._root);
         htmlExtractor.filterByNumber(4);
 
-        ArrayList<ArrayList<Node>> records = htmlExtractor.records;
+        ArrayList<ArrayList<Node>> records = htmlExtractor._results;
 
         return;
     }
+
+
+    /* Input: */
+    private Node _root;
+
+    /* Parameters:  */
+    private float _proximity = 0.7f;
+
+    /* Output: */
+    private ArrayList<ArrayList<Node>> _results = new ArrayList<ArrayList<Node>>();
+
+    /* Tools:  */
+    private BracketStringInputParser _APTEDParser = new BracketStringInputParser();
+    private APTED<StringUnitCostModel, StringNodeData> _apted = new APTED<>(new StringUnitCostModel());
+
+    /**
+     * Constructor
+     * @param root: the root of the input DOM tree
+     */
+    public HTMLExtractor(Node root) {
+        this._root = root;
+    }
+
+    /**
+     * Customize the proximity
+     * @param proximity
+     */
+    public void setProximity(float proximity) {
+        this._proximity = proximity;
+    }
+    public double getProximity() {
+        return _proximity;
+    }
+
+
+    /**
+     * Traverse the tree (pre-order) to extract _results from the HTML doc.
+     * Call method on root, if its children are hit _results, move to its
+     * Sibling. If not, call method on all its child recursively one by one.
+     * @param root the root node of the DOM representation of the HTML doc.
+     */
+    public void extractData(Node root) {
+        int childNodeSize = root.childNodeSize();
+
+        if (childNodeSize <= 0) {
+            return;
+
+        } else if (childNodeSize == 1){
+            extractData(root.childNode(0));
+
+        } else {
+            if (addToResults(root) > 0) {
+                return;
+
+            } else {
+                for (Node node : root.childNodes()) {
+                    extractData(node);
+                }
+            }
+        }
+
+        return;
+    }
+
+
+    /**
+     * Decide whether a node's children are the _results to output. If they are,
+     * they will be put in an arraylist. And the arraylist will be one element
+     * of the class' member results.
+     * @param node whose children are examined.
+     * @return the number of results to output. If it is 0, this node's children
+     * are not the results users are interested in.
+     */
+    private int addToResults(Node node) {
+        ArrayList<Node> outputList = new ArrayList<Node>();
+        int childNodeSize = node.childNodeSize();
+
+        boolean[] voteResult;
+
+        System.out.println(node.numOffSpring + ":");
+
+        voteResult = vote(node);
+        for(int i = 0; i < childNodeSize; i++) {
+            if(voteResult[i]) {
+                outputList.add(node.childNode(i));
+            }
+        }
+
+        if (outputList.isEmpty()) {
+            return 0;
+        } else {
+            _results.add(outputList);
+            return outputList.size();
+        }
+
+    }
+
+
+    /**
+     * The node's children will vote for each other based on _proximity. If two
+     * children are close enough, they will give each other one vote. If one c-
+     * hild get enough votes, its vote result is true. That means it is one of
+     * the possible results which the user is interested in.
+     * @param node
+     * @return the vote result for each child.
+     */
+    private boolean[] vote(Node node) {
+        float voteThreshold = 0.6f; //TODO: modularize
+
+        /* Input: */
+        int childNodeSize = node.childNodeSize();
+
+        /* Output: */
+        int[] voteCount = new int[childNodeSize];
+        boolean[] voteResult = new boolean[childNodeSize];
+
+        /* Vote for each node */
+        for(int i = 0; i < childNodeSize; i++) {
+            for(int j = i + 1; j < childNodeSize; j++) {
+                /* Check for null String */
+                if (node.childNode(i).APTEDTreeStructure == null || node.childNode(j).APTEDTreeStructure == null) {
+                    continue;
+                }
+
+                /* Compute TED */
+                apted.node.Node<StringNodeData> t1 = _APTEDParser.fromString(node.childNode(i).APTEDTreeStructure);
+                apted.node.Node<StringNodeData> t2 = _APTEDParser.fromString(node.childNode(j).APTEDTreeStructure);
+
+                /* Judge if TED exceed threshold */
+                float TEDthreshold = (node.childNode(i).numOffSpring + node.childNode(j).numOffSpring) / 2
+                        * (1 - _proximity);
+                //System.out.println("i"+i+"j"+j+"tmp"+tmp+"distance"+_apted.computeEditDistance(t1, t2));
+                if (TEDthreshold >= _apted.computeEditDistance(t1, t2)) {
+                    voteCount[i]++;
+                    voteCount[j]++;
+                }
+            }
+        }
+
+        /* Collect voting results */
+        for(int i = 0; i < childNodeSize; i++) {
+            System.out.print(voteCount[i]);
+            if (voteCount[i] >= voteThreshold * childNodeSize) {
+                voteResult[i] = true;
+            } else {
+                voteResult[i] = false;
+            }
+        }
+
+        return voteResult;
+    }
+
+    /**
+     * Filter the results given the minimum result size
+     * @param minResultSize
+     * @return
+     */
+    public boolean filterByNumber(int minResultSize) {
+        if (minResultSize < 1) {
+            throw new IllegalArgumentException("Min result size should be larger than 0!");
+        }
+
+        for(int i = 0; i < _results.size(); i++) {
+            if(_results.get(i).size() < minResultSize) {
+                _results.remove(i);
+                i--;
+            }
+        }
+        return true;
+    }
+
 }
